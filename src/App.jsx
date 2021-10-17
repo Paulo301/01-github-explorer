ccf3f89157218c2b022abc8fc94c7531a6d99dcd
Paulo301 <paulovictor1918@gmail.com>
2021-10-16 22:05:51 -0300

    M2 - Estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { RepositoryList } from './components/RepositoryList';
+import { Counter } from './components/Counter';
+
 import './styles/global.scss';
 
 export function App() {
   // throw new Error("Erro");
 
-  return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
